352691a4cb3061da0aa6a9d8e76555c2794e6005
GreenRamSilgen <kiranshrs@yahoo.com>
2025-05-02 07:27:16 -0700

    Initial Commit

---
+++ 100Habit/_00HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00HabitApp.swift
+//  100Habit
+//
+//  Created by Kiran Shrestha on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct _00HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
